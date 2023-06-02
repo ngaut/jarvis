cd5197fe016d5343e2f880b725c06c9030016fb9
goroutine <ngaut@126.com>
2023-06-02 00:38:43 -0700

    tiny clean up

--- smartgpt/planner.py
+++ smartgpt/planner.py
@@ -1,10 +1,7 @@
 from typing import Optional
 from dotenv import load_dotenv
-from spinner import Spinner
-import actions, gpt
-import os, sys, time, re, signal, argparse, logging
-import ruamel.yaml as yaml
-from datetime import datetime
+import time, logging
+import gpt
 
 
 def gen_instructions(model: str):
@@ -13,10 +10,8 @@ def gen_instructions(model: str):
 
     try:
         logging.info("========================")
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         prompt = (
-            f"## Current time: {current_time}\n"
-            f"our goal: {goal}.\n"
+            f"our goal: {goal}\n\n"
             "your json response:```json"
         )
         resp = gpt.complete_with_system_message(prompt, model=model)
