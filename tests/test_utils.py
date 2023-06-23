2bcebaf2040ef3ee8583c74b57fdc16bdbdf4a0a
Ken Liu <liuy@pingcap.com>
2023-06-23 20:48:41 +0800

    Setup test cases for Interpreter module

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -83,5 +83,33 @@ class TestUtils(unittest.TestCase):
         expected = '{"kvs":[{"key":"key_points_1.seq3.list", "value":"<to_fill>"}]}'
         self.assertEqual(eval_expression(text), expected)
 
+    @patch('smartgpt.jvm.get', return_value=1)
+    def test_eval_without_eval(self, mock_get):
+        text = '{"kvs":[{"key":"key_points_" + str(jvm.get("idx")) + ".seq3.list", "value":"<to_fill>"}]}'
+        expected = None
+        self.assertEqual(eval_expression(text), expected)
+
+    @patch('smartgpt.jvm.get', return_value=0)
+    def test_multiple_nested_evals(self, mock_get):
+        text = '@eval("key_points_" + "@eval(str(jvm.get("@eval("idx")")))" + ".seq@eval(str(1+1+1)).list")'
+        expected = 'key_points_0.seq3.list'
+        result = eval_expression(text)
+        result = eval_expression(result)
+        result = eval_expression(result)
+        result = eval_expression(result)
+        self.assertEqual(result, expected)
+
+    @patch('smartgpt.jvm.get', return_value=0)
+    def test_eval_expression_complex(self, mock_get):
+        text = '{"kvs":[{"key":"@eval("key_points_" + str(jvm.get("idx")) + ".seq"+ str(@eval(@eval("2*" + str(jvm.get("idx"))))) + ".list")", "value":"<to_fill>"}]}'
+        expected_step_1 = '{"kvs":[{"key":"@eval("key_points_" + str(jvm.get("idx")) + ".seq"+ str(@eval(2*0)) + ".list")", "value":"<to_fill>"}]}'
+        expected_step_2 = '{"kvs":[{"key":"@eval("key_points_" + str(jvm.get("idx")) + ".seq"+ str(0) + ".list")", "value":"<to_fill>"}]}'
+        expected_step_3 = '{"kvs":[{"key":"key_points_0.seq0.list", "value":"<to_fill>"}]}'
+
+        self.assertEqual(eval_expression(text), expected_step_1)
+        self.assertEqual(eval_expression(expected_step_1), expected_step_2)
+        self.assertEqual(eval_expression(expected_step_2), expected_step_3)
+
+
 if __name__ == "__main__":
     unittest.main()
