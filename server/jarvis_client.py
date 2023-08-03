a1ee6671d45e05b27d2c7947e87562bd8d8268ae
IANTHEREAL <argregoryian@gmail.com>
2023-08-03 13:44:16 +0800

    fix grpc dev test client

--- server/jarvis_client.py
+++ server/jarvis_client.py
@@ -3,9 +3,9 @@ import server.jarvis_pb2 as jarvis_pb2
 import server.jarvis_pb2_grpc as jarvis_pb2_grpc
 
 def run():
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('localhost:51153')
     stub = jarvis_pb2_grpc.JarvisStub(channel)
-    response = stub.Execute(jarvis_pb2.ExecuteRequest(task="what's tidb"))
+    response = stub.Execute(jarvis_pb2.ExecuteRequest(task="tell me what's tidb"))
     print("Jarvis client received: " + response.result)
 
 if __name__ == '__main__':
