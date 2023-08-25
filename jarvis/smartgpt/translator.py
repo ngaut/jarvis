b328c2d079894be386d360a5eac432ddaf7304dc
iamxy <liuy@pingcap.com>
2023-08-25 10:59:56 +0800

    Improve reviewer performance and add reviser role to correct instructions. (#47)
    * Add role of reviser.
    
    * Fix bug in gen_plan().
    
    * Update syntax review prompt.
    
    * Make reviewer more focus.
    
    * Improve missing eval() check.
    
    * Fix simulation reviewer.
    
    * Fix syntax reviwer.
    
    * Update reviewers.
    
    * Cleanup code.

--- jarvis/smartgpt/translator.py
+++ jarvis/smartgpt/translator.py
@@ -10,10 +10,13 @@ from jarvis.smartgpt import reviewer
 
 REVIEWER_CLASSES = [
     (reviewer.EvalSyntaxReviewer, [gpt.GPT_3_5_TURBO_16K]),
-    (reviewer.LoopIndexKeyReviewer, [gpt.GPT_3_5_TURBO_16K])]
+    (reviewer.LoopIndexKeyReviewer, [gpt.GPT_3_5_TURBO_16K]),
+]
 
-#REVIEWER_GPT4_CLASSES = [(reviewer.SimulationReviewer, [gpt.GPT_4])]
-REVIEWER_GPT4_CLASSES = []
+REVIEWER_GPT4_CLASSES = [
+    (reviewer.SyntaxReviewer, [gpt.GPT_4]),
+    (reviewer.SimulationReviewer, [gpt.GPT_4]),
+]
 
 FEW_SHOT_EXAMPLE = "3"
 
@@ -27,7 +30,7 @@ class Translator:
         messages.append({"role": "system", "content": preprompts.get("translator_sys") + "\n" + fewshot.get(FEW_SHOT_EXAMPLE)})
         return messages
 
-    def translate_to_instructions(self, task_info: Dict[str, Any]):
+    def prepare_user_hints(self, task_info: Dict[str, Any]):
         hints = ""
         if task_info.get("first_task", False):
             hints = "\n  - \"This is the first task, so there are no previous tasks or outcomes.\""
@@ -43,7 +46,33 @@ class Translator:
 
         if not hints:
             hints = "[]"
+        return hints
 
+    def revise_instructions(self, task_info: Dict[str, Any], instrs, review_results, review_comments):
+        review_feedbacks = []
+        for i, (result, comment) in enumerate(zip(review_results, review_comments)):
+            if result is False:
+                review_feedbacks.append(comment)
+
+        if len(review_feedbacks) == 0:
+            logging.info("No revision required.")
+            return instrs
+
+        messages = []
+        messages.append({"role": "system", "content": preprompts.get("reviser_sys")})
+        messages.append({"role": "user", "content": preprompts.get("reviser_user").format(
+            instructions = instrs,
+            review_feedback="\n\n".join(review_feedbacks),
+        )})
+
+        resp = gpt.send_messages(messages, self.model)
+        messages.append({"role": "asssistant", "content": resp})
+        self._trace_reviser_gen(task_info, messages)
+
+        return utils.strip_yaml(resp)
+
+    def translate_to_instructions(self, task_info: Dict[str, Any]):
+        hints = self.prepare_user_hints(task_info)
         user_prompt = preprompts.get("translator_user").format(
             task_num=task_info.get("task_num", 0),
             task = f"\"{task_info.get('task', '')}\"",
@@ -51,6 +80,7 @@ class Translator:
             start_seq = task_info.get("start_seq", ""),
             hints = hints,
         )
+
 
         logging.info(f"User Prompt: \n{user_prompt}")
 
@@ -64,11 +94,15 @@ class Translator:
         resp = utils.strip_yaml(resp)
         logging.info(f"LLM Response: \n{resp}")
 
-        for rev in self.reviewers:
-            resp, messages = rev.review(resp)
-            self._trace_reviewer_gen(task_info, rev, messages)
+        review_results = []
+        review_comments = []
+        for rv in self.reviewers:
+            result, comment, messages = rv.review(resp)
+            self._trace_reviewer_gen(task_info, rv, messages)
+            review_results.append(result)
+            review_comments.append(comment)
 
-        return resp
+        return self.revise_instructions(task_info, resp, review_results, review_comments)
 
     def _trace_llm_gen(self, task_info, messages):
         with open(f"review_{task_info.get('task_num', 0)}.txt", "w") as f:
@@ -76,11 +110,19 @@ class Translator:
                 f.write(f"{msg['role'].upper()}:\n")
                 f.write(f"{msg['content']}\n\n")
 
-
-    def _trace_reviewer_gen(self, task_info, rev, messages):
+    def _trace_reviewer_gen(self, task_info, rv, messages):
         with open(f"review_{task_info.get('task_num', 0)}.txt", "a") as f:
             f.write("=============================================\n")
-            f.write(f"Reviewer: {rev.__class__.__name__}\n")
+            f.write(f"Reviewer: {rv.__class__.__name__}\n")
+            f.write("=============================================\n")
+            for msg in messages:
+                f.write(f"{msg['role'].upper()}:\n")
+                f.write(f"{msg['content']}\n\n")
+
+    def _trace_reviser_gen(self, task_info, messages):
+        with open(f"review_{task_info.get('task_num', 0)}.txt", "a") as f:
+            f.write("=============================================\n")
+            f.write("Reviser\n")
             f.write("=============================================\n")
             for msg in messages:
                 f.write(f"{msg['role'].upper()}:\n")
