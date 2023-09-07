6edbdd21e86056565c86a690318cacf29bcaec45
IANTHEREAL <argregoryian@gmail.com>
2023-09-07 18:54:59 +0800

    add skill name

--- jarvis/server/jarvis_server.py
+++ jarvis/server/jarvis_server.py
@@ -137,9 +137,10 @@ class JarvisServicer(jarvis_pb2_grpc.JarvisServicer, JarvisAgent):
                 error="skill_id is not provided",
             )
         agent_id = request.agent_id.strip()
+        skill_name = request.skill_name.strip()
 
         try:
-            skill_name = self.agent.save_skill(agent_id)
+            skill_name = self.agent.save_skill(agent_id, skill_name)
         except Exception as e:
             return jarvis_pb2.SaveSkillResponse(
                 agent_id=agent_id,
